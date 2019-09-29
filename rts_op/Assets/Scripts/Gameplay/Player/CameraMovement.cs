16073c114307b905ed70c762ee4770ddd44711f7
PeterDorade <peterdorade@gmail.com>
2019-09-29 22:42:17 +0200

    working on camera movement. first working hud.

---
+++ rts_op/Assets/Scripts/Gameplay/Player/CameraMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float cameraSpeed = 10;
+    void Update()
+    {
+        if (Input.mousePosition.x <= 1)
+        {
+            transform.localPosition += new Vector3(-cameraSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.mousePosition.x >= Screen.width - 1)
+        {
+            transform.localPosition += new Vector3(cameraSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.mousePosition.y <= 1)
+        {
+            transform.localPosition += new Vector3(0, -Mathf.Cos(-130) * Mathf.Sin(40) * cameraSpeed * Time.deltaTime, 0);
+        }
+        if (Input.mousePosition.y >= Screen.height - 1)
+        {
+            transform.localPosition += new Vector3(0, Mathf.Sin(-130) *cameraSpeed * Time.deltaTime, Mathf.Sin(40) * cameraSpeed * Time.deltaTime); 
+        }
+    }
+}
+
--- rts_op/Assets/Scripts/Gameplay/Player/HUD.cs
+++ rts_op/Assets/Scripts/Gameplay/Player/HUD.cs
@@ -7,33 +7,16 @@ public class HUD : MonoBehaviour
 {
     public  void OnGUI()
     {
-        PlayerManager playerManager = GetComponent<PlayerManager>();
+        PlayerManager playerManager = GetComponentInParent<PlayerManager>();
         FieldInfo[] fields = playerManager.playerData.GetType().GetFields();
+        int fieldNumber = 0;
         foreach (FieldInfo field in fields)
         {
-            bool firstFieldNamed;
-            string nameFirstField;
-            string fieldName = field.Name;
-            int fieldNumber;
-            int numberOfStrings;
-            if (firstFieldNamed = false)
-            {
-                nameFirstField = fieldName;
-                firstFieldNamed = true;
-                fieldNumber = 1;
-            }
-            if (nameFirstField == fieldName)
-            {
-                numberOfStrings = fieldNumber;
-            }
-            else
-            {
-                fieldNumber = fieldNumber + 1;
-                object fieldValue = field.GetValue(playerManager.playerData);
-                string fieldString = fieldValue.ToString();
-                GUI.Label(new Rect(new Vector2(20, 10*fieldNumber), new Vector2(100, 100)), fieldString);
-            }
-            
+            fieldNumber++;
+            string fieldValueString = field.GetValue(playerManager.playerData).ToString();
+            string fieldNameString = field.Name;
+            string fieldString =  fieldNameString.ToUpper() + ": " + fieldValueString;
+            GUI.Label(new Rect(new Vector2(20, 10*fieldNumber), new Vector2(100, 100)), fieldString);
         }
     }
-}
+}   
