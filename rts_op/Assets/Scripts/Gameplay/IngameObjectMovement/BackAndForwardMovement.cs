64fdb625180aa9c6a80db0eb7c2da0b6e8bd2ce0
PeterDorade <peterdorade@gmail.com>
2019-10-06 02:18:14 +0200

    lvlqbase

---
+++ rts_op/Assets/Scripts/Gameplay/IngameObjectMovement/BackAndForwardMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackAndForwardMovement : MonoBehaviour
+{
+    public float objectMovementSpeed = 30f;
+    public Vector3 objectMovement;
+    private Vector3 currentObjectMovement;
+    private Vector3 startPosition;
+    private float objectMovementSpeedPerFrame;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+        currentObjectMovement = objectMovement;
+        objectMovementSpeedPerFrame = objectMovementSpeed * Time.deltaTime;
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(startPosition, transform.position) >= objectMovement.magnitude)
+        {
+            currentObjectMovement = -objectMovement;
+        }
+        if (Vector3.Distance(startPosition + objectMovement, transform.position) >= objectMovement.magnitude)
+        {
+            currentObjectMovement = objectMovement;
+        }
+        transform.position += objectMovementSpeedPerFrame * currentObjectMovement;
+    }
+}
